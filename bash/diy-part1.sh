61077a21f821fb0bd873395ba423689a1a5f46b8
name <mail@example.com>
2023-06-19 10:46:58 -0400

    first commit

---
+++ bash/diy-part1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# kernel
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+# sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=6.1/g' target/linux/x86/Makefile
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall
+
+# luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/tmp
+mv package/tmp/luci-app-openclash package/
+rm -rf package/tmp/
---
+++ bash/diy-part2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+#
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+#
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/luci/luci-theme-argon-mod
+
+# luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/feeds/luci/luci-theme-argon
+
+# rm package/base-files/files/etc/banner
+# wget https://github.com/bXZb/release/raw/main/etc/banner -O package/base-files/files/etc/banner

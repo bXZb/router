061a39dcd651a1a77914b7b81d6b4c99f8a0d37a
admin <you@example.com>
2024-03-12 11:25:42 +0800

    upload

--- depends/diy-part2.sh
+++ depends/diy-part2.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+
 
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
@@ -24,4 +15,7 @@ rm -rf package/feeds/luci/luci-theme-argon-mod
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/feeds/luci/luci-theme-argon
 
 rm package/base-files/files/etc/banner
-wget https://raw.githubusercontent.com/bXZb/router/master/bash/banner -O package/base-files/files/etc/banner
+wget https://raw.githubusercontent.com/bXZb/router/master/depends/banner -O package/base-files/files/etc/banner
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate

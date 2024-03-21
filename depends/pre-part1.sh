2af18a439ee843a9042f7009f2b459b346b8c651
bXZb <89792508+bXZb@users.noreply.github.com>
2024-03-21 15:01:31 +0800

    Add files via upload

---
+++ depends/pre-part1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# kernel
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.15/KERNEL_TESTING_PATCHVER:=6.1/g' target/linux/x86/Makefile
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
+echo 'src-git tailscale https://github.com/asvow/luci-app-tailscale.git' >>feeds.conf.default
---
+++ depends/pre-part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+#
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+
+#
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/luci/luci-theme-argon-mod
+
+# luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/feeds/luci/luci-theme-argon
+
+rm package/base-files/files/etc/banner
+wget https://raw.githubusercontent.com/bXZb/router/master/depends/banner -O package/base-files/files/etc/banner
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate

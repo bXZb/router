fecee1d8ba3bc6ee443b704dbdab6ab57ea9cb5e
bXZb <89792508+bXZb@users.noreply.github.com>
2024-08-24 18:59:49 +0800

    Update diy-part1.sh

--- depends/diy-part1.sh
+++ depends/diy-part1.sh
@@ -5,8 +5,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # kernel
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
-sed -i 's/KERNEL_TESTING_PATCHVER:=6.6/KERNEL_TESTING_PATCHVER:=6.1/g' target/linux/x86/Makefile
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' target/linux/x86/Makefile
+#sed -i 's/KERNEL_TESTING_PATCHVER:=6.6/KERNEL_TESTING_PATCHVER:=6.1/g' target/linux/x86/Makefile
 
 # Add a feed source
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
